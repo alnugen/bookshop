b59b9f7104162933a7ce9b844dab30784efe6f43
alnugen <lim.nugen@gmail.com>
2016-04-01 09:48:22 +0545

    Users and sessSession added for authorised login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,27 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
   resources :line_items
   resources :carts
   get 'store/index'
-
+  resources :products do
+    get :who_bought, on: :member
+  end
   resources :products
 
   # You can have the root of your site routed with "root"
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -16,6 +16,7 @@ class OrdersControllerTest < ActionController::TestCase
     assert_redirected_to store_path
     assert_equal flash[:notice], 'Your cart is empty'
   end
+
   test "should get new" do
     item = LineItem.new
     item.build_cart
@@ -31,7 +32,7 @@ class OrdersControllerTest < ActionController::TestCase
       post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     end
 
-    assert_redirected_to order_path(assigns(:order))
+    assert_redirected_to store_path
   end
 
   test "should show order" do
